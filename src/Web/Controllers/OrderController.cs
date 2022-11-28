bd5a4991dcc9572b5c29a71ae61cd23abf4f2c11
chin <kushaochin@163.com>
2022-11-28 23:17:18 +0800

    ApiExplorerSettings、IMediator

--- src/Web/Controllers/OrderController.cs
+++ src/Web/Controllers/OrderController.cs
@@ -7,11 +7,12 @@ using Microsoft.eShopWeb.Web.Features.OrderDetails;
 
 namespace Microsoft.eShopWeb.Web.Controllers;
 
-[ApiExplorerSettings(IgnoreApi = true)]
+[ApiExplorerSettings(IgnoreApi = true)]//@对外隐藏该接口
 [Authorize] // Controllers that mainly require Authorization still use Controller/View; other pages use Pages
 [Route("[controller]/[action]")]
 public class OrderController : Controller
 {
+    //@IMediator：定义一个中介来封装请求/响应和发布交互模式
     private readonly IMediator _mediator;
 
     public OrderController(IMediator mediator)
